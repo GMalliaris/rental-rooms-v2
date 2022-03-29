ddde5f2862bd47683723f22a7ba04ad5821db977
GMalliaris <georgemalliaris8@gmail.com>
2022-03-29 23:37:32 +0300

    #3: removed security hotspot

--- src/main/java/org/gmalliaris/rental/rooms/config/exception/ExceptionControllerAdvice.java
+++ src/main/java/org/gmalliaris/rental/rooms/config/exception/ExceptionControllerAdvice.java
@@ -1,5 +1,7 @@
 package org.gmalliaris.rental.rooms.config.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
@@ -15,31 +17,32 @@ import java.util.TreeMap;
 @ResponseBody
 public class ExceptionControllerAdvice {
 
+    private static final Logger logger = LoggerFactory.getLogger(ExceptionControllerAdvice.class);
+
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(Exception.class)
     public ExceptionResponse exception(Exception exception) {
-        exception.printStackTrace();
+        logException(exception);
         return new ExceptionResponse(HttpStatus.INTERNAL_SERVER_ERROR, exception.getMessage());
     }
 
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     @ExceptionHandler(AccessDeniedException.class)
     public ExceptionResponse accessDeniedException(AccessDeniedException exception) {
-        exception.printStackTrace();
+        logException(exception);
         return new ExceptionResponse(HttpStatus.UNAUTHORIZED, "Access is denied.");
     }
 
     @ExceptionHandler(ApiException.class)
     public ResponseEntity<ExceptionResponse> apiException(ApiException apiException){
-        apiException.printStackTrace();
+        logException(apiException);
         return ResponseEntity.status(apiException.getStatus())
                 .body(new ExceptionResponse(apiException.getStatus(), apiException.getMessage()));
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ExceptionMapResponse> methodArgumentNotValidException(MethodArgumentNotValidException methodArgumentNotValidException){
-        methodArgumentNotValidException.printStackTrace();
-
+        logException(methodArgumentNotValidException);
         var fieldErrorMap = new TreeMap<String, String>();
         methodArgumentNotValidException.getBindingResult()
                 .getFieldErrors()
@@ -49,6 +52,10 @@ public class ExceptionControllerAdvice {
         return ResponseEntity.status(400)
                 .body(new ExceptionMapResponse(HttpStatus.BAD_REQUEST,
                         fieldErrorMap));
+    }
+
+    private void logException(Throwable exception){
+        logger.info("Handled exception:", exception);
     }
 }
 
