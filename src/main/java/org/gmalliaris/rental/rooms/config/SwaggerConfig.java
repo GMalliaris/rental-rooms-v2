7dcbf7b4588c7b1db11365f1db6842c8df70ae9c
GMalliaris <georgemalliaris8@gmail.com>
2022-08-21 17:28:02 +0300

    #13 - added swagger urls to permit list, added swagger OpenApi bean

---
+++ src/main/java/org/gmalliaris/rental/rooms/config/SwaggerConfig.java
@@ -0,0 +1,18 @@
+package org.gmalliaris.rental.rooms.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("RentalRooms API v2")
+                        .description("Rental Rooms sample application")
+                        .version("v0.0.1"));
+    }
+}
--- src/main/java/org/gmalliaris/rental/rooms/config/WebSecurityConfig.java
+++ src/main/java/org/gmalliaris/rental/rooms/config/WebSecurityConfig.java
@@ -2,23 +2,23 @@ package org.gmalliaris.rental.rooms.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig {
 
     private static final String[] PERMITTED_ENDPOINTS = new String[]{"/auth/register",
             "/auth/login", "/auth/confirm/**"};
+    public static final String[] SWAGGER_ENDPOINTS = new String[]{ "/swagger-ui/**", "/v3/api-docs/**"};
     private final WebSecurityConfigService serviceConfig;
     private final CustomAuthenticationEntryPointConfig customAuthenticationEntryPointConfig;
 
@@ -27,33 +27,22 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         this.customAuthenticationEntryPointConfig = customAuthenticationEntryPointConfig;
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
                 .antMatchers(PERMITTED_ENDPOINTS).permitAll()
+                .antMatchers(SWAGGER_ENDPOINTS).permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .exceptionHandling()
                 .authenticationEntryPoint(customAuthenticationEntryPointConfig)
                 .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        serviceConfig.configure(http);
+
+        serviceConfig.configureHttpSecurityJwtFilter(http);
+
+        return http.build();
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        serviceConfig.configure(auth, bCryptPasswordEncoder());
-    }
-
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Override
-    @Bean
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
 }
